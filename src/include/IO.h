6f5605de3b87f6d9859368e97983f7390207ee76
LiangJM <jntmngmhahayo@outlook.com>
2025-06-02 13:06:30 +0800

    改了一下，fw gcc，神人🤣

--- src/include/IO.h
+++ src/include/IO.h
@@ -34,7 +34,24 @@
 #ifdef IO_H
 #define IO_H
 
-static inline void outb(unsigned short port, unsigned char val);
-static inline unsigned char inb(unsigned short port);
+// Write a byte to an I/O port
+static inline void outb(unsigned short port, unsigned char val) {
+    __asm__ volatile (
+        "outb %0, %1"
+        :
+        : "a"(val), "Nd"(port)
+    );
+}
+
+// Read a byte from an I/O port
+static inline unsigned char inb(unsigned short port) {
+    unsigned char ret;
+    __asm__ volatile (
+        "inb %1, %0"
+        : "=a"(ret)
+        : "Nd"(port)
+    );
+    return ret;
+}
 
 #endif
