3d5d1d6fe5815b2ee2177cf5c25d4af910b0ef19
LiangJM <jntmngmhahayo@outlook.com>
2025-06-05 19:32:43 +0800

    fs fat32 in VOS!

--- src/kernels/fs/fat32.c
+++ src/kernels/fs/fat32.c
@@ -29,4 +29,67 @@
  * <jntmngmhahayo@gmail.com>
  * 
  */
-// TODO: 以后再搞
+// FAT32 - fat32.c
+#include <stdint.h>
+
+// FAT32引导扇区（BPB）结构体
+typedef struct {
+    uint8_t  jump_boot[3];
+    uint8_t  oem_name[8];
+    uint16_t bytes_per_sector;
+    uint8_t  sectors_per_cluster;
+    uint16_t reserved_sector_count;
+    uint8_t  num_fats;
+    uint16_t root_entry_count;
+    uint16_t total_sectors_16;
+    uint8_t  media;
+    uint16_t fat_size_16;
+    uint16_t sectors_per_track;
+    uint16_t num_heads;
+    uint32_t hidden_sectors;
+    uint32_t total_sectors_32;
+    // FAT32扩展
+    uint32_t fat_size_32;
+    uint16_t ext_flags;
+    uint16_t fs_version;
+    uint32_t root_cluster;
+    uint16_t fs_info;
+    uint16_t backup_boot_sector;
+    uint8_t  reserved[12];
+    uint8_t  drive_number;
+    uint8_t  reserved1;
+    uint8_t  boot_signature;
+    uint32_t volume_id;
+    uint8_t  volume_label[11];
+    uint8_t  fs_type[8];
+} __attribute__((packed)) FAT32_BPB;
+
+// FAT32目录项结构体
+typedef struct {
+    uint8_t  name[11];
+    uint8_t  attr;
+    uint8_t  ntres;
+    uint8_t  crt_time_tenth;
+    uint16_t crt_time;
+    uint16_t crt_date;
+    uint16_t last_access_date;
+    uint16_t first_cluster_high;
+    uint16_t write_time;
+    uint16_t write_date;
+    uint16_t first_cluster_low;
+    uint32_t file_size;
+} __attribute__((packed)) FAT32_DirEntry;
+
+// 读取FAT表项
+uint32_t fat32_read_fat_entry(uint8_t* fat, uint32_t cluster) {
+    // FAT表每项4字节
+    uint32_t entry_offset = cluster * 4;
+    return *(uint32_t*)(fat + entry_offset) & 0x0FFFFFFF; // 低28位有效
+}
+
+// 设置FAT表项
+void fat32_write_fat_entry(uint8_t* fat, uint32_t cluster, uint32_t value) {
+    uint32_t entry_offset = cluster * 4;
+    uint32_t* entry = (uint32_t*)(fat + entry_offset);
+    *entry = (*entry & 0xF0000000) | (value & 0x0FFFFFFF);
+}
