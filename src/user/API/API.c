61c228bf2f429aa59cd4305aef6e775a10ba7aae
LiangJM <jntmngmhahayo@outlook.com>
2025-06-10 20:09:09 +0800

    滚木

--- src/user/API/API.c
+++ src/user/API/API.c
@@ -6,10 +6,6 @@
  * - This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  * 
- * But if you want to shut down? No way! Please to
- * - Mental hospital
- * - FSF (Free Software Foundation)
- * - Court
  * 
  * - cn v :
  * 版权所有 (C) 2025 ~ ? Ai-Upace
@@ -19,10 +15,6 @@
  * - 本程序是自由软件：您可以根据 GNU 通用公共许可证的条款重新分发和/或修改它
  * - 由自由软件基金会发布的 GNU 通用公共许可证
  * 
- * 但如果你想闭源？没门！请去;
- * - 精神病院
- * - 自由软件基金会 (FSF)
- * - 法院
  * 
  * END
  * <jntmngmhahayo@outlook.com>
